9b0362f9ce93702db10f87b35e95468202516afc
Peter Ashraf <peterashrafmail@gmail.com>
2024-06-22 18:19:48 +0300

    commit

--- socketServer.js
+++ socketServer.js
@@ -30,7 +30,7 @@ app.use(cors());
 
 mongoose.connect("mongodb+srv://Peter:TZ8eXrltEYMuVdQb@cluster0.f1z37qq.mongodb.net/Graduation_Project")
     .then(() => {
-        Server.listen(PORT, IP,  () => {
+        Server.listen(PORT,  () => {
 
             app.get("/", (req, res) => {
                 res.send("hello to server")
